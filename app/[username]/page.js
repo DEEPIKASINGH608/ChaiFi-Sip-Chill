137f1e77e9d161ccbb81b929dad9b01f5de49528
DEEPIKASINGH608 <ds0648775@gmail.com>
2026-04-23 20:41:11 +0530

    Add some more features

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -3,28 +3,20 @@ import React from 'react'
 const Username = ({ params }) => {
   return (
     <>
-      <div className='flex flex-col items-center'>
-        {params.username}
-      </div>
-
+      {params.username}
       <div className='cover w-full bg-red-50 relative'>
-        {/* Added 'px' to the height and fixed the path if necessary */}
-        <img
-          className='object-cover w-full h-[360px]'
-          src="/patreon_banner.gif"
-          alt="Banner"
-        />
-        <div>
-          <img src="" alt="" />
+        <img className='object-cover w-full h-[350]' src="/patreon_banner.gif" alt="" />
+        <div className='absolute -bottom-14 right-[50%] border-white border'>
+          <img width={115} height={115} src="https://www.vecteezy.com/free-photos/cute-baby-cat" alt="" />
         </div>
-      </div>
-
-      <div className='cover'>
-        {params.username}
       </div>
     </>
   )
 }
 
 export default Username
+
+
+
+
 
