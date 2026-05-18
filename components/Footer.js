ca960bac1f226df6e8bc4f05531e4e14a5f072fb
DEEPIKASINGH608 <ds0648775@gmail.com>
2026-05-18 09:15:21 +0530

    Update footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='bg-[#020617] border-b border-white/5 shadow-2xl text-center p-4 h-16 mt-auto'>
         <p className='text-sm text-gray-200'>
-            &copy; {new Date().getFullYear()} ChaiFi. All rights reserved.
+            &copy; {new Date().getFullYear()} ChaiFi. All rights reserved. | <a href="/privacy" className='text-blue-500 hover:underline'>Privacy Policy</a> | <a href="/terms" className='text-blue-500 hover:underline'>Terms of Service</a>
         </p>
     </footer>
   )
